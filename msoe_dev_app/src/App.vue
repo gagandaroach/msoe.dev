7c519e296b129dc171a24ad8b494bbbbbf0e48bf
gagany <gagandaroach@gmail.com>
2019-03-17 12:57:41 -0500

    add header elementui component, remove basic header component

--- msoe_dev_app/src/App.vue
+++ msoe_dev_app/src/App.vue
@@ -1,26 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
+    <MsoeHeader />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MsoeHeader from './components/MsoeHeader.vue'
+
 console.log("Starting instance @ " + Date(Date.now()));
+
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    MsoeHeader
   }
 }
 </script>
